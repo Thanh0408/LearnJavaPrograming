b8018c8aae71716d14950dee4a0b425d13e46be3
Thanh0408 <quangthanh.hust@gmail.com>
2022-01-19 12:39:59 +0700

    delete fail project

--- Mini_Project_Springboot/BasicSpringBootWeb/src/main/java/tsdv/basicspringbootweb/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package tsdv.basicspringbootweb.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tsdv.basicspringbootweb.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
--- Mini_Project_Springboot/BasicSpringBootWeb/src/main/java/tsdv/basicspringbootweb/repository/ShopRepository.java
+++
@@ -1,7 +0,0 @@
-package tsdv.basicspringbootweb.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tsdv.basicspringbootweb.entity.Shop;
-
-public interface ShopRepository extends JpaRepository<Shop, Long> {
-}
